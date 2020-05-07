8d3cd00fd1b19b820b1c05adac2615b4285b1e96
GoonerBrian <brian.hoblin@gmail.com>
2020-05-07 19:36:16 -0400

    Added comments

--- helloGitHub.cpp
+++ helloGitHub.cpp
@@ -4,6 +4,13 @@ int main()
 {
     std::cout << "Hello GitHub!" << std::endl;
     std::cout << "This is an update." << std::endl;
-    std::cout << "This is another update." <<std::endl;
+    std::cout << "This is another update." << std::endl;
     return 0;
 }
+
+// echo "# <repo-name>" >> README.md
+// git init
+// git add README.md
+// git commit -m "first commit"
+// git remote add origin git@github.com:<user-name>/<repo-name>.git
+// git push -u origin master
